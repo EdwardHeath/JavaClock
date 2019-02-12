955e20db434e9346ae04b6715008e558c08f09e8
Edward Heath <e42291+github@gmail.com>
2019-02-11 21:48:08 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let hourHand = document.getElementById('hour-hand')
+let minuteHand = document.getElementById('minute-hand')
+let secondHand = document.getElementById('second-hand')
+let time = document.getElementById("time");
+let date = document.getElementById('date');
+
+function setTime() {
+  let now = new Date();
+
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  let period = hour > 12 ? "PM" : "AM";
+
+  let hourDegrees = (hour * 30) + (minute * 0.5) + 90;
+  let minuteDegrees = (minute * 6) + (second * 0.1) + 90;
+  let secondDegrees = (second * 6) + 90;
+
+  fixFlicker(hourHand, hourDegrees);
+  fixFlicker(minuteHand, minuteDegrees);
+  fixFlicker(secondHand, secondDegrees);
+
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  let dayOfWeek = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'][now.getDay()];
+  let month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][now.getMonth()];
+  let day = now.getDate();
+  let year = now.getFullYear();
+
+  // converts from 24 hour clock
+  hour = hour == 0 ? 12 : (hour > 12 ? hour - 12 : hour);
+
+  hour = hour < 10 ? `0${hour}` : `${hour}`;
+  minute = minute < 10 ? `0${minute}` : `${minute}`;
+  second = second < 10 ? `0${second}` : `${second}`;
+
+  time.innerHTML = `${hour}:${minute}:${second} ${period}`;
+
+  date.innerHTML = `${dayOfWeek} ${month} ${day} ${year}`
+
+  document.title = time.innerHTML;
+}
+
+function fixFlicker(hand,degrees) {
+    if (degrees == 90) {
+        hand.style.transition = 'all 0.0s';
+    }
+}
+
+setTime();
+
+setInterval(setTime, 1000);
